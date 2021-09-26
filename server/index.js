70ea064f4f7117dc909873bd5b3be89d42460a09
Abhishek Vaishnav <abhishekprasadvaishnav@gmail.com>
2021-09-26 16:01:05 +0530

    Adding static build through express

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // Allow Cross Origin
 const cors = require('cors');
@@ -9,6 +10,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+app.use('/', express.static(path.join(__dirname, 'public/dist')));
 
 // Connect Cluster DB
 mongoose.connect('mongodb+srv://admin:admin@cluster0.kxuzx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
@@ -20,6 +26,10 @@ app.use('/graphql', graphqlHTTP.graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+app.get('/*', function(req, res, next) {
+  res.sendFile(__dirname + '/public/dist/index.html');
+});
 
 app.listen(3000, () =>  {
   console.log('Server is running!');
